5846416a58950fa5e6f1a970a7c053a35b5b5711
KONFeature <quentin@nivelais.com>
2023-05-05 17:16:42 +0200

    SST-Monorepo init

---
+++ packages/functions/src/lambda.ts
@@ -0,0 +1,8 @@
+import { ApiHandler } from "sst/node/api";
+import { Time } from "@cashmere-monorepo-v2/core/time";
+
+export const handler = ApiHandler(async (_evt) => {
+  return {
+    body: `Hello world. The time is ${Time.now()}`,
+  };
+});
