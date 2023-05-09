38b7bebb6f643a35fcff909cd9406e89d7f8164f
KONFeature <quentin@nivelais.com>
2023-05-10 01:54:48 +0200

    ⚡️ Optimise runtime lambda perf of the type compiler

--- backend/core/src/middleware/typeboxValidator.ts
+++ backend/core/src/middleware/typeboxValidator.ts
@@ -1,6 +1,6 @@
 import middy from '@middy/core';
 import { TObject, TProperties } from '@sinclair/typebox';
-import { Value } from '@sinclair/typebox/value';
+import { TypeCompiler } from '@sinclair/typebox/compiler';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 /**
@@ -13,16 +13,21 @@ export const typeboxValidatorMiddleware = <
     requestEventSchema: TObject<Input>;
     responseEventSchema: TObject<Output>;
 }): middy.MiddlewareObj<APIGatewayProxyEvent, APIGatewayProxyResult> => {
+    // Build our type compiler (this avoid too much type generation and save runtime perf on multiple lambda call's)
+    const requestTypeCompiler = TypeCompiler.Compile(types.requestEventSchema);
+    const responseTypeCompiler = TypeCompiler.Compile(
+        types.responseEventSchema
+    );
+
+    // Pre lambda call, ensuring validity of the input
     const before: middy.MiddlewareFn<
         APIGatewayProxyEvent,
         APIGatewayProxyResult
     > = async (request): Promise<void> => {
         // Ensure the request match the input
-        if (!Value.Check(types.requestEventSchema, request.event)) {
+        if (!requestTypeCompiler.Check(request.event)) {
             // Get the error's
-            const errors = [
-                ...Value.Errors(types.requestEventSchema, request.event),
-            ];
+            const errors = [...requestTypeCompiler.Errors(request.event)];
             // Throw an error
             // TODO: Custom error with status code: 422
             throw new Error(
@@ -33,16 +38,15 @@ export const typeboxValidatorMiddleware = <
         }
     };
 
+    // Post lambda execution check, ensuring validity of the typing's
     const after: middy.MiddlewareFn<
         APIGatewayProxyEvent,
         APIGatewayProxyResult
     > = async (request): Promise<void> => {
         // Ensure the response match the output
-        if (!Value.Check(types.responseEventSchema, request.response)) {
+        if (!responseTypeCompiler.Check(request.response)) {
             // Get the error's
-            const errors = [
-                ...Value.Errors(types.responseEventSchema, request.response),
-            ];
+            const errors = [...responseTypeCompiler.Errors(request.response)];
             // Throw an error
             // TODO: Custom error, status code 500
             throw new Error(
