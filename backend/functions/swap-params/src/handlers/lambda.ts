1172fea461dca13a68ed7819e9a8655d496b457e
Kiran Pachhai <kiran.pachhai@gmail.com>
2023-05-13 08:24:07 -0400

    Added trpc  support  for  lambda.ts

--- backend/functions/swap-params/src/handlers/lambda.ts
+++ backend/functions/swap-params/src/handlers/lambda.ts
@@ -1,8 +1,57 @@
-import { ApiHandler } from 'sst/node/api';
+import { middyWithLog } from '@cashmere-monorepo/backend-core';
+import { typeboxValidatorMiddleware } from '@cashmere-monorepo/backend-core/middleware/typeboxValidator';
+import httpErrorHandler from '@middy/http-error-handler';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpResponseSerializer from '@middy/http-response-serializer';
+import { Type } from '@sinclair/typebox';
+import {
+    CreateAWSLambdaContextOptions,
+    awsLambdaRequestHandler,
+} from '@trpc/server/adapters/aws-lambda';
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { publicProcedure, router } from './trpc';
 
-export const handler = ApiHandler(async (_evt) => {
-    return {
-        statusCode: 200,
-        body: `Hello world from swap estimate functions.`,
-    };
+// Create your router
+const appRouter = router({
+    'test-lambda': publicProcedure.query(async () => {
+        return `Hello world from swap estimate functions.`;
+    }),
 });
+
+// created for each request
+const createContext = ({
+    event,
+    context,
+}: CreateAWSLambdaContextOptions<APIGatewayProxyEventV2>) => ({}); // no context
+// type Context = trpc.inferAsyncReturnType<typeof createContext>
+
+// Create the base handler
+export const baseHandler = awsLambdaRequestHandler({
+    router: appRouter,
+    createContext,
+});
+
+/* const eventResponse = Type.Object({
+    body: Type.Any(),
+}); */
+
+export const handler = middyWithLog(baseHandler)
+    .use(httpEventNormalizer())
+    .use(
+        httpResponseSerializer({
+            serializers: [
+                {
+                    regex: /^application\/json$/,
+                    serializer: ({ body }) => JSON.stringify(body),
+                },
+            ],
+            defaultContentType: 'application/json',
+        })
+    )
+    .use(
+        typeboxValidatorMiddleware({
+            requestEventSchema: Type.Object({}), // No input parameters
+            responseEventSchema: Type.Object({}),
+        })
+    )
+    .use(httpErrorHandler());
