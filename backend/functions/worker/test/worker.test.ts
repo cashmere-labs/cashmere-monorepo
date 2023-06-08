3fef42057181fa726c1de12eb47ee59313760f03
Salimtb <salim.toubal@consensys.net>
2023-06-08 18:04:19 +0200

    feat: add test to the worker

---
+++ backend/functions/worker/test/worker.test.ts
@@ -0,0 +1,45 @@
+import { Template } from 'aws-cdk-lib/assertions';
+import { App, getStack } from 'sst/constructs';
+import { initProject } from 'sst/project';
+import { describe, expect, it } from 'vitest';
+import { WorkerStack } from '../WorkerStack';
+
+describe('WorkerStack', () => {
+    it('should be defined', () => {
+        expect(WorkerStack).toBeDefined();
+    });
+
+    it('should create WorkerStack', async () => {
+        await initProject({});
+        const app = new App({ mode: 'deploy' });
+
+        // WHEN
+        app.stack(WorkerStack);
+
+        // THEN
+        const template = Template.fromStack(getStack(WorkerStack));
+        template.hasResourceProperties('AWS::Lambda::Function', {
+            Timeout: 300,
+            MemorySize: 1024,
+            Runtime: 'nodejs16.x',
+        });
+
+        template.hasResourceProperties('AWS::SQS::Queue', {
+            VisibilityTimeout: 1810,
+            MessageRetentionPeriod: 604800,
+            QueueName: 'dev-my-app-TxSenderQueue',
+        });
+
+        template.hasResourceProperties('AWS::Events::Rule', {
+            ScheduleExpression: 'rate(2 minute)',
+        });
+
+        template.hasResourceProperties('AWS::Events::Rule', {
+            ScheduleExpression: 'rate(5 minute)',
+        });
+        template.hasOutput('TxSenderQueueUrl', {});
+        template.hasOutput('TxSenderQueueId', {});
+        template.hasOutput('BridgeCronId', {});
+        template.hasOutput('SupervisorCronId', {});
+    });
+});
