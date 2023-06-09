897a9977495d9aa3d12753c33447b9a0d5971c0c
pavelshmk <shumkovpav@gmail.com>
2023-06-09 19:59:58 +0300

    ✅ add coverage reports to config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
     test: {
         coverage: {
             provider: 'istanbul',
+            reporter: ['html', 'text', 'json', 'json-summary'],
         },
     },
 });
