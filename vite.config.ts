4b72cbc4f27ef0ca7854d8abedbf410ea1007a33
pavelshmk <shumkovpav@gmail.com>
2023-06-09 20:10:14 +0300

    ✅ set coverage thresholds

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
         coverage: {
             provider: 'istanbul',
             reporter: ['html', 'text', 'json', 'json-summary'],
+            lines: 80,
+            branches: 80,
+            functions: 80,
+            statements: 80,
         },
     },
 });
