384ed04dd83b69db578cededbe51ad97e3d50e2d
KONFeature <quentin@nivelais.com>
2023-06-24 20:47:08 +0200

    💚 Fix estimate services test

--- backend/services/swap/test/params/estimate.test.ts
+++ backend/services/swap/test/params/estimate.test.ts
@@ -1,4 +1,10 @@
-import { describe, expect, it } from 'vitest';
+import {
+    ONE_INCH_SLIPPAGE,
+    getL0ChainFromChainId,
+    getNetworkConfig,
+    isPlaceholderToken,
+} from '@cashmere-monorepo/backend-blockchain';
+import { afterEach, beforeAll, describe, expect, it, vi } from 'vitest';
 import { swapEstimation } from '../../src';
 
 /**
@@ -16,17 +22,67 @@ const mockedSwapInput = {
  * Swap estimate business logic test
  */
 describe('[Swap][Logic] Estimate', () => {
+    let swapEstimationTest: typeof swapEstimation;
+
+    /**
+     * Before all tests, mock all dependencies and extract our method's
+     */
+    beforeAll(async () => {
+        // Mock the blockchain repository repository
+        vi.doMock('@cashmere-monorepo/backend-blockchain', () => ({
+            getAssetRepository: () => ({
+                tokenSymbol: async () => 'ETH',
+                tokenDecimal: async () => 18,
+            }),
+            getAssetRouterRepository: () => ({
+                quoteSwaps: async () => ({
+                    potentialOutcome: 1n,
+                    haircut: 1n,
+                    minPotentialOutcome: 1n,
+                }),
+                getPoolTokenAsset: async () =>
+                    '0x86738E8b53e14449Cc4A7EFBb1F701bFA3c19b08',
+            }),
+            getBridgeRepository: () => ({
+                getSwapFeeL0: async () => 1n,
+            }),
+            getUniswapRepository: () => ({
+                getAmountOut: async () => ({
+                    minDstAmount: 1n,
+                    dstAmount: 2n,
+                }),
+            }),
+            // Some flat pass from initial implementation
+            isPlaceholderToken,
+            getNetworkConfig,
+            getL0ChainFromChainId,
+            ONE_INCH_SLIPPAGE,
+        }));
+
+        // Import the tested functions after mocking dependencies
+        const tmpImport = await import('../../src');
+        swapEstimationTest = tmpImport.swapEstimation;
+    });
+
+    /**
+     * After each test, restore all mocks
+     */
+    afterEach(() => {
+        // Restore all mocks
+        vi.restoreAllMocks();
+    });
+
     it(`[Fail] Invalid network's`, async () => {
         // Invalid src chain id
         await expect(async () =>
-            swapEstimation({
+            swapEstimationTest({
                 ...mockedSwapInput,
                 srcChainId: 0,
             })
         ).rejects.toThrowError();
         // Invalid dst chain id
         await expect(async () =>
-            swapEstimation({
+            swapEstimationTest({
                 ...mockedSwapInput,
                 dstChainId: 0,
             })
@@ -35,7 +91,7 @@ describe('[Swap][Logic] Estimate', () => {
 
     it('[Ok] Should succeed with mocked input', async () => {
         // Pass a valid mocked input and test the result
-        const result = await swapEstimation(mockedSwapInput);
+        const result = await swapEstimationTest(mockedSwapInput);
         // Ensure we got all the right properties
         expect(result).toBeDefined();
         expect(result).toHaveProperty('dstAmount');
