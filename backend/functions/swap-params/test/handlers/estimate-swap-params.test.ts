1f8fc2f7f45ae8113e5fd4330ef96acf43dfecd8
KONFeature <quentin@nivelais.com>
2023-06-24 20:00:56 +0200

    💚 Fix test on estimate function services

--- backend/functions/swap-params/test/handlers/estimate-swap-params.test.ts
+++ backend/functions/swap-params/test/handlers/estimate-swap-params.test.ts
@@ -1,72 +1,95 @@
-import { Api } from 'sst/node/api';
-import { describe, expect, it } from 'vitest';
+import { afterEach, beforeAll, describe, expect, it, vi } from 'vitest';
+import { handler } from '../../src/handlers/estimate-swap-params';
 
 /**
  * Swap estimate business logic test
  */
 describe('[Swap][Endpoint] Estimate', () => {
-    // The api url we will use for our call
-    const baseEndpoint = `${Api.SwapApi.url}estimate`;
-    const baseRequest = {
-        headers: new Headers(),
-        method: 'GET',
-    };
+    const estimateSwapMock = vi.fn(() => ({
+        dstAmount: '1',
+        minReceivedDst: '2',
+        fee: '3',
+        priceImpact: '4',
+        nativeFee: '5',
+    }));
 
-    // Build a query url
-    const buildUrl = (params: Record<string, string>) =>
-        `${baseEndpoint}?${new URLSearchParams(params)}`;
+    let handlerToTest: typeof handler;
+
+    beforeAll(async () => {
+        vi.doMock('@cashmere-monorepo/backend-service-swap', () => ({
+            swapEstimation: estimateSwapMock,
+        }));
+
+        handlerToTest = (
+            await import('../../src/handlers/estimate-swap-params')
+        ).handler;
+    });
+
+    afterEach(() => {
+        // Reset the mocks
+        vi.clearAllMocks();
+    });
 
     // Ensure it fail if we don't provide any input param
     it('[Fail] No input param', async () => {
-        const result = await fetch(baseEndpoint, baseRequest);
-        expect(result.status).toBe(400);
-    });
-
-    // Ensure it fail if we don't use the adequate method
-    it("[Fail] Don't exist with wrong method ", async () => {
-        const result = await fetch(baseEndpoint, {
-            ...baseRequest,
-            method: 'POST',
-        });
-        expect(result.status).toBe(404);
+        // @ts-ignore
+        const result = await handlerToTest({}, {});
+        expect(result.statusCode).toBe(400);
     });
 
     // Ensure it fail if we don't provide all required params
     it('[Fail] Not all param are present', async () => {
-        const url = buildUrl({
-            srcChainId: '59140',
-            srcToken: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
-            dstChainId: '80001',
-            dstToken: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
-        });
-        const result = await fetch(url, baseRequest);
-        expect(result.status).toBe(400);
+        const result = await handlerToTest(
+            // @ts-ignore
+            {
+                queryStringParameters: {
+                    srcChainId: '10050',
+                    srcToken: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+                    dstChainId: '10051',
+                    dstToken: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+                },
+            },
+            {}
+        );
+        expect(result.statusCode).toBe(400);
     });
 
     // Ensure it doesn't fail if we provide extra params
     it("[Ok] Don't fail with extra param", async () => {
-        const url = buildUrl({
-            srcChainId: '59140',
-            srcToken: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
-            amount: '100000000000000000000',
-            dstChainId: '80001',
-            dstToken: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
-            extra: 'extra',
-        });
-        const result = await fetch(url, baseRequest);
-        expect(result.status).toBe(200);
+        const result = await handlerToTest(
+            // @ts-ignore
+            {
+                queryStringParameters: {
+                    srcChainId: '10050',
+                    srcToken: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+                    amount: '100000000000000000000',
+                    dstChainId: '10051',
+                    dstToken: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+                    extra: 'extra',
+                },
+            },
+            {}
+        );
+        expect(estimateSwapMock).toHaveBeenCalledOnce();
+        expect(result.statusCode).toBe(200);
     });
 
     // Ensure it executes successfully with correct params
     it("[Ok] Pass with good param's", async () => {
-        const url = buildUrl({
-            srcChainId: '59140',
-            srcToken: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
-            amount: '100000000000000000000',
-            dstChainId: '80001',
-            dstToken: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
-        });
-        const result = await fetch(url, baseRequest);
-        expect(result.status).toBe(200);
+        const result = await handlerToTest(
+            // @ts-ignore
+            {
+                queryStringParameters: {
+                    srcChainId: '10050',
+                    srcToken: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+                    amount: '100000000000000000000',
+                    dstChainId: '10050',
+                    dstToken: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+                },
+            },
+            {}
+        );
+        expect(estimateSwapMock).toHaveBeenCalledOnce();
+        expect(result.statusCode).toBe(200);
     });
 });
