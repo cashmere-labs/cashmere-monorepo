f494cf13189473d14cceb3e97ef94b7df0265184
pavelshmk <shumkovpav@gmail.com>
2023-07-06 15:46:50 +0300

    ♻️ import frontend package

---
+++ frontend/src/constants/utils.ts
@@ -0,0 +1,5 @@
+export const NUMBER_REGEX = /^-?\d*\.?\d*$/;
+
+export const IS_PROD = !import.meta.env.DEV;
+export const apiDomain = !IS_PROD ? 'localhost:3003' : location.host;
+export const apiAddress = `//${apiDomain}`;
---
+++ frontend/src/utils/formatBalance.ts
@@ -0,0 +1,22 @@
+import { BigNumberish, ethers } from 'ethers';
+import Big from 'big.js';
+
+export const formatBalance = (balance: BigNumberish | Big | undefined, decimals = 4, tokenDecimal = 18) => {
+  if (!balance) {
+    return '0';
+  }
+
+  balance = balance.toString();
+
+  try {
+    const _balance = ethers.utils.formatUnits(balance as BigNumberish, tokenDecimal).toString();
+
+    const parts = _balance.split('.');
+    if (parts[1]) parts[1] = parts[1].slice(0, decimals);
+    const returned = parts.join('.');
+    return returned;
+  } catch (err) {
+    console.log(err);
+    return '';
+  }
+};
